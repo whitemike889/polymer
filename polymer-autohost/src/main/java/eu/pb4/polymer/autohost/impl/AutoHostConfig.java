7388ba96ff3b86b6e0eda6771bba8a116a7b711e
Patbox <39821509+Patbox@users.noreply.github.com>
2024-12-08 11:31:44 +0100

    Cleanup comments in autohost config

--- polymer-autohost/src/main/java/eu/pb4/polymer/autohost/impl/AutoHostConfig.java
+++ polymer-autohost/src/main/java/eu/pb4/polymer/autohost/impl/AutoHostConfig.java
@@ -18,22 +18,23 @@ public class AutoHostConfig {
     public String _c2 = "Marks resource pack as required";
     @SerializedName("required")
     public boolean require = false;
-    public String _c3 = "Mods may override the above setting and make the resource pack required, set this to false to disable that.";
+    public String _c7 = "Mods may override the above setting and make the resource pack required, set this to false to disable that.";
     @SerializedName("mod_override")
     public boolean modOverride = true;
-    public String _c4 = "Type of resource pack provider. Default: 'polymer:automatic'";
+    public String _c3 = "Type of resource pack provider. Default: 'polymer:automatic'";
     public String type = "polymer:automatic";
-    public String _c5 = "Configuration of type, see provider's source for more details";
+    public String _c4 = "Configuration of type, see provider's source for more details";
     @SerializedName("settings")
     public JsonElement providerSettings = new JsonObject();
-    public String _c6 = "Message sent to clients before pack is loaded";
+    public String _c5 = "Message sent to clients before pack is loaded";
     public JsonElement message = new JsonPrimitive("This server uses resource pack to enhance gameplay with custom textures and models. It might be unplayable without them.");
-    public String _c7 = "Disconnect message in case of failure";
+    public String _c6 = "Disconnect message in case of failure";
     @SerializedName("disconnect_message")
     public JsonElement disconnectMessage = new JsonPrimitive("Couldn't apply server resourcepack!");
-
+    public String _c8 = "Allows to define more external resource packs. It's an object with \"id\" for uuid, \"url\" for the pack url and \"hash\" for the SHA1 hash.";
     @SerializedName("external_resource_packs")
     public List<ExternalResourcePack> externalResourcePacks = new ArrayList<>();
+    public String _c9 = "Moves resource pack generation earlier when running on server. Might break some mods.";
     @SerializedName("setup_early")
     public boolean loadEarly = CommonImpl.DEV_ENV;
 
